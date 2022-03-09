9b24bdc90ff2be8e8db5d37909b791e2c7815b5c
Azoies1 <le_thom@live.concordia.ca>
2022-03-09 14:17:24 -0500

    Download jdk 17 for this to work

--- src/RobotControllerTest.java
+++ src/RobotControllerTest.java
@@ -4,9 +4,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Arrays;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
+//import org.junit.After;
+//import org.junit.Before;
+//import org.junit.Rule;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;

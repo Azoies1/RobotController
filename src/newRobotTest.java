3c67bd5b63baa7e5ee4d9a0498682b894e35dedd
Azoies1 <le_thom@live.concordia.ca>
2022-03-10 12:16:06 -0500

    new test file

---
+++ src/newRobotTest.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+
+//import org.junit.After;
+//import org.junit.Before;
+//import org.junit.Rule;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class newRobotTest {
+
+    @Test
+    void apple(){}
+}
